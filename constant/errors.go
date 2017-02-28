5e545419d1e4244924086e9675bd2ccfeaedf138
bruce <bruce@aeris.io>
2017-02-28 18:30:54 +0800

    add constant

---
+++ constant/errors.go
@@ -0,0 +1,8 @@
+package constant
+
+const (
+	CMD_INPUT_ERROR     = "I can't understand your hart"
+	PATH_UNEXISTS_ERROR = "I can't find your little secret"
+	CMD_ERROR           = "Look...I still can't understand your hart"
+	RUN_ERROR           = "e...I don't know waht's wrong"
+)
