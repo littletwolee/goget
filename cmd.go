fd0089747b6e54ad71b0d020adaa7d3800e50379
littletwolee <littletwolee@gmail.com>
2017-05-16 13:10:33 +0800

    add build/watch/cmd module

--- cmd.go
+++ cmd.go
@@ -1,23 +1,53 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"os/exec"
+	"github.com/littletwolee/commons"
+	"os"
+	"strings"
 )
 
-func ExecCommand(cmd *exec.Cmd, isStdout bool) (string, error) {
-	var buf bytes.Buffer
-	cmd.Stdout = &buf
-	cmd.Stderr = &buf
-	if isStdout {
-		cmd.Stdout = os.Stdout // 重定向标准输出
-		cmd.Stderr = os.Stderr // 重定向标准输出
-	}
-	err := cmd.Run()
-	out := buf.Bytes()
+func Build() (string, error) {
+	_, strerr, err := commons.GetCmd().ExecCommand("go", "./", []string{"build"}, false)
 	if err != nil {
-		return fmt.Sprintf("%s", out), err
+		return "", err
 	}
-	return fmt.Sprintf("%s", out), nil
+	return strerr, nil
+}
+
+func Get(output string) {
+	for _, v := range strings.Split(output, "\n") {
+		if strings.Contains(v, "in any of") {
+			pack := strings.Split(v, `"`)[1]
+			if len(strings.Split(pack, `/`)) > 3 {
+				packarr := strings.Split(pack, `/`)
+				pack = fmt.Sprintf(`%s/%s/%s`, packarr[0], packarr[1], packarr[2])
+			}
+			command := "git"
+			pars := []string{"clone", "--progress", fmt.Sprintf(`https://%s`, pack), pack}
+			rootdir := fmt.Sprintf(`%s/src/`, os.ExpandEnv("$GOPATH"))
+			commons.GetLogger().OutMsg(fmt.Sprintf("Downloading %s", pack))
+			commons.GetCmd().ExecCommand(command, rootdir, pars, true)
+			commons.GetLogger().OutMsg(fmt.Sprintf("Download %s success", pack))
+		}
+	}
+
+}
+
+func Run() {
+	dir, _, err := commons.GetCmd().ExecCommand("pwd", "./", nil, false)
+	var (
+		projectName string
+	)
+	if err != nil {
+		commons.GetLogger().OutErr(err)
+	}
+	if strings.Contains(dir, "/") {
+		projectName = dir[strings.LastIndex(dir, "/")+1:]
+	}
+	buildstr, err := Build()
+	if buildstr != "" {
+		Get(buildstr)
+	}
+
 }
--- watch.go
+++ watch.go
@@ -1,9 +1,42 @@
 package main
 
-// watch ...
+import (
+	"github.com/go-fsnotify/fsnotify"
+	"github.com/littletwolee/commons"
+)
+
 func Watch() {
-	for {
-
+	output, _ := Build()
+	if output != "" {
+		Get(output)
 	}
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		commons.GetLogger().OutErr(err)
+	}
+	defer watcher.Close()
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case _ = <-watcher.Events:
+				output, _ := Build()
+				if output != "" {
+					Get(output)
+				}
+			case err := <-watcher.Errors:
+				commons.GetLogger().OutErr(err)
+			}
+		}
+	}()
 
+	err = watcher.Add("slog")
+	if err != nil {
+		commons.GetLogger().OutErr(err)
+	}
+	<-done
+
+	if err != nil {
+		commons.GetLogger().OutErr(err)
+	}
 }
